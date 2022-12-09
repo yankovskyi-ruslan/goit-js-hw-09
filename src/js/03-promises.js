11089fa9511dc316f201fb02baf7d82b5cabe6ff
yankovskyi-ruslan <tital3108@gmail.com>
2022-12-09 13:32:31 +0200

    виконує 3 завдання ДЗ

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,44 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix';
+
+const promisesForm = document.querySelector('.form');
+promisesForm.addEventListener('submit', onPromisesFormSubmit);
+
+function onPromisesFormSubmit(e) {
+  e.preventDefault();
+  const { amount, delay, step } = e.target.elements;
+  const submitButton = e.target.lastElementChild
+  let delayTime = Number(delay.value)
+  // Спосіб заборони зайвого кліку по формі під час виконання функції за допомогою відключення кнопки на час виконання функції
+  submitButton.disabled = true;
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delayTime)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delayTime += Number(step.value);
   }
+
+  // Спосіб заборони зайвого кліку по формі під час виконання функції за допомогою очищення форми
+  // e.target.reset();
+
+  // Спосіб заборони зайвого кліку по формі під час виконання функції за допомогою відключення кнопки на час виконання функції
+  setTimeout(() => { 
+    submitButton.disabled = false;
+  }, delayTime)
+}
+
+function createPromise(position, delay) {
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
